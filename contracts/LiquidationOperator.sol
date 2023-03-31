f6fa703e861a069df44ae60d5d48acaccc2efed2
Paradorn-248 <paradorn248@gmail.com>
2023-03-31 23:26:56 +0700

    question3 finished

--- contracts/LiquidationOperator.sol
+++ contracts/LiquidationOperator.sol
@@ -135,21 +135,19 @@ contract LiquidationOperator is IUniswapV2Callee {
 
     // TODO: define constants used in the contract including ERC-20 tokens, Uniswap Pairs, Aave lending pools, etc. */
 
-    IERC20 constant WBTC = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
     IWETH constant WETH = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IERC20 constant USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    IERC20 constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     IUniswapV2Factory constant uniswapV2Factory =
         IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
-    IUniswapV2Pair immutable uniswapV2Pair_WBTC_USDT; // Pool1
-    IUniswapV2Pair immutable uniswapV2Pair_WBTC_WETH; // Pool2
+    IUniswapV2Pair immutable uniswapV2Pair_WETH_USDC; // Pool1
 
     ILendingPool constant lendingPool =
         ILendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
 
     address constant liquidationTarget =
-        0x59CE4a2AC5bC3f5F225439B2993b86B42f6d3e9F;
-    uint debt_USDT;
+        0x63f6037d3e9d51ad865056BF7792029803b6eEfD;
+    uint debt_USDC;
 
     // END TODO
 
@@ -194,12 +192,9 @@ contract LiquidationOperator is IUniswapV2Callee {
     constructor() {
         // TODO: (optional) initialize your contract
 
-        uniswapV2Pair_WBTC_USDT = IUniswapV2Pair(
-            uniswapV2Factory.getPair(address(WBTC), address(USDT))
+        uniswapV2Pair_WETH_USDC = IUniswapV2Pair(
+            uniswapV2Factory.getPair(address(WETH), address(USDC))
         ); // Pool1
-        uniswapV2Pair_WBTC_WETH = IUniswapV2Pair(
-            uniswapV2Factory.getPair(address(WBTC), address(WETH))
-        ); // Pool2
 
         // END TODO
     }
@@ -211,7 +206,7 @@ contract LiquidationOperator is IUniswapV2Callee {
     // END TODO
 
     // required by the testing script, entry for your liquidation call
-    function operate(uint256 _debt_USDT) external {
+    function operate(uint256 _debt_USDC) external {
         // TODO: implement your liquidation logic
 
         // 0. security checks and initializing variables
@@ -244,9 +239,9 @@ contract LiquidationOperator is IUniswapV2Callee {
         // we know that the target user borrowed USDT with WBTC as collateral
         // we should borrow USDT, liquidate the target user and get the WBTC, then swap WBTC to repay uniswap
         // (please feel free to develop other workflows as long as they liquidate the target user successfully)
+        debt_USDC = _debt_USDC;
 
-        debt_USDT = _debt_USDT;
-        uniswapV2Pair_WBTC_USDT.swap(0, debt_USDT, address(this), "$");
+        uniswapV2Pair_WETH_USDC.swap(debt_USDC, 0, address(this), "$");
 
         // 3. Convert the profit into ETH and send back to sender
 
@@ -260,59 +255,37 @@ contract LiquidationOperator is IUniswapV2Callee {
     // required by the swap
     function uniswapV2Call(
         address,
+        uint256 amount0,
         uint256,
-        uint256 amount1,
         bytes calldata
     ) external override {
         // TODO: implement your liquidation logic
 
         // 2.0. security checks and initializing variables
 
-        assert(msg.sender == address(uniswapV2Pair_WBTC_USDT));
-        (
-            uint256 reserve_WBTC_Pool1,
-            uint256 reserve_USDT_Pool1,
-
-        ) = uniswapV2Pair_WBTC_USDT.getReserves(); // Pool1
-        (
-            uint256 reserve_WBTC_Pool2,
-            uint256 reserve_WETH_Pool2,
-
-        ) = uniswapV2Pair_WBTC_WETH.getReserves(); // Pool2
+        assert(msg.sender == address(uniswapV2Pair_WETH_USDC));
+        (uint256 reserve_USDC, uint256 reserve_WETH, ) = uniswapV2Pair_WETH_USDC
+            .getReserves();
 
         // 2.1 liquidate the target user
 
-        uint debtToCover = amount1;
-        USDT.approve(address(lendingPool), debtToCover);
+        uint debtToCover = amount0;
+        USDC.approve(address(lendingPool), debtToCover);
         lendingPool.liquidationCall(
-            address(WBTC),
-            address(USDT),
+            address(WETH),
+            address(USDC),
             liquidationTarget,
             debtToCover,
             false
         );
-        uint collateral_WBTC = WBTC.balanceOf(address(this));
-        console.log("collateral_WBTC:", collateral_WBTC);
+        uint collateral_WETH = WETH.balanceOf(address(this));
+        console.log("collateral_WETH:", collateral_WETH);
 
-        //repay
-        uint repay_WBTC = getAmountIn(
-            debt_USDT,
-            reserve_WBTC_Pool1,
-            reserve_USDT_Pool1
-        );
-        console.log("repay_WBTC:", repay_WBTC);
-        WBTC.transfer(address(uniswapV2Pair_WBTC_USDT), repay_WBTC);
-        // now we will transfer WBTC TO WETH
-        uint profit_WBTC = WBTC.balanceOf(address(this));
-        console.log("profit_WBTC:", profit_WBTC);
+        // repay
 
-        WBTC.transfer(address(uniswapV2Pair_WBTC_WETH), profit_WBTC);
-        uint amountOut_WETH = getAmountOut(
-            profit_WBTC,
-            reserve_WBTC_Pool2,
-            reserve_WETH_Pool2
-        );
-        uniswapV2Pair_WBTC_WETH.swap(0, amountOut_WETH, address(this), "");
+        uint repay_WETH = getAmountIn(debtToCover, reserve_WETH, reserve_USDC);
+        WETH.transfer(address(uniswapV2Pair_WETH_USDC), repay_WETH);
+
         // END TODO
     }
 }
--- test/liquidation.js
+++ test/liquidation.js
@@ -1,26 +1,26 @@
 const { expect } = require('chai')
 const { network, ethers } = require('hardhat')
 const { BigNumber, utils } = require('ethers')
-const { writeFile } = require('fs')
 
-describe('Liquidation question 2', function () {
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    it('2000 USDT liquidation', async function () {
+describe('Liquidation question3', function () {
+    // for (let i = 0; i < 10; i++) {
+    it('test', async function () {
         await network.provider.request({
             method: 'hardhat_reset',
             params: [
                 {
                     forking: {
                         jsonRpcUrl: process.env.ALCHE_API,
-                        blockNumber: 12489619,
+                        blockNumber: 11946807,
                     },
                 },
             ],
         })
 
         const gasPrice = 0
-        const debt_USDT = ethers.utils.parseUnits('2000', 6)
-
+        const debt_in_USDC = 8556
+        const debt_USDC = ethers.utils.parseUnits(debt_in_USDC.toString(), 6)
+        console.log('debt_USDC:', debt_in_USDC.toString())
         const accounts = await ethers.getSigners()
         const liquidator = accounts[0].address
 
@@ -40,7 +40,7 @@ describe('Liquidation question 2', function () {
         await liquidationOperator.deployed()
 
         const liquidationTx = await liquidationOperator.operate(
-            debt_USDT,
+            debt_USDC,
             (overrides = { gasPrice: gasPrice })
         )
         const liquidationReceipt = await liquidationTx.wait()
@@ -59,7 +59,7 @@ describe('Liquidation question 2', function () {
         const expectedLiquidationEvents = liquidationReceipt.logs.filter(
             (v) =>
                 v.topics[3] ===
-                '0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f'
+                '0x00000000000000000000000063f6037d3e9d51ad865056bf7792029803b6eefd'
         )
 
         expect(
@@ -82,115 +82,5 @@ describe('Liquidation question 2', function () {
 
         expect(profit.gt(BigNumber.from(0)), 'not profitable').to.be.true
     })
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    it('5000 USDT liquidation', async function () {
-        await network.provider.request({
-            method: 'hardhat_reset',
-            params: [
-                {
-                    forking: {
-                        jsonRpcUrl: process.env.ALCHE_API,
-                        blockNumber: 12489619,
-                    },
-                },
-            ],
-        })
-
-        const gasPrice = 0
-        const debt_USDT = ethers.utils.parseUnits('5000', 6)
-
-        const accounts = await ethers.getSigners()
-        const liquidator = accounts[0].address
-
-        const beforeLiquidationBalance = BigNumber.from(
-            await hre.network.provider.request({
-                method: 'eth_getBalance',
-                params: [liquidator],
-            })
-        )
-
-        const LiquidationOperator = await ethers.getContractFactory(
-            'LiquidationOperator'
-        )
-        const liquidationOperator = await LiquidationOperator.deploy(
-            (overrides = { gasPrice: gasPrice })
-        )
-        await liquidationOperator.deployed()
-
-        const liquidationTx = await liquidationOperator.operate(
-            debt_USDT,
-            (overrides = { gasPrice: gasPrice })
-        )
-        const liquidationReceipt = await liquidationTx.wait()
-
-        const liquidationEvents = liquidationReceipt.logs.filter(
-            (v) =>
-                v &&
-                v.topics &&
-                v.address === '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9' &&
-                Array.isArray(v.topics) &&
-                v.topics.length > 3 &&
-                v.topics[0] ===
-                    '0xe413a321e8681d831f4dbccbca790d2952b56f977908e45be37335533e005286'
-        )
-
-        const expectedLiquidationEvents = liquidationReceipt.logs.filter(
-            (v) =>
-                v.topics[3] ===
-                '0x00000000000000000000000059ce4a2ac5bc3f5f225439b2993b86b42f6d3e9f'
-        )
-
-        expect(
-            expectedLiquidationEvents.length,
-            'no expected liquidation'
-        ).to.be.above(0)
-        expect(liquidationEvents.length, 'unexpected liquidation').to.be.equal(
-            expectedLiquidationEvents.length
-        )
-
-        const afterLiquidationBalance = BigNumber.from(
-            await hre.network.provider.request({
-                method: 'eth_getBalance',
-                params: [liquidator],
-            })
-        )
-
-        const profit = afterLiquidationBalance.sub(beforeLiquidationBalance)
-        console.log('Profit', utils.formatEther(profit), 'ETH')
-
-        expect(profit.gt(BigNumber.from(0)), 'not profitable').to.be.true
-    })
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    it('10000 USDT liquidation', async function () {
-        await network.provider.request({
-            method: 'hardhat_reset',
-            params: [
-                {
-                    forking: {
-                        jsonRpcUrl: process.env.ALCHE_API,
-                        blockNumber: 12489619,
-                    },
-                },
-            ],
-        })
-
-        const gasPrice = 0
-        const debt_USDT = ethers.utils.parseUnits('10000', 6)
-
-        const LiquidationOperator = await ethers.getContractFactory(
-            'LiquidationOperator'
-        )
-        const liquidationOperator = await LiquidationOperator.deploy(
-            (overrides = { gasPrice: gasPrice })
-        )
-        await liquidationOperator.deployed()
-
-        await expect(
-            liquidationOperator.operate(
-                debt_USDT,
-                (overrides = { gasPrice: gasPrice })
-            )
-        ).to.be.reverted
-        console.log('Transaction reverted. No profit.')
-    })
+    // }
 })
